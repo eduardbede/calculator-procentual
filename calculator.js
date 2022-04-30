e0980be6a9d897c46c3dab625bf0c145a7b1957a
eduardbede <bede.eduard@gmail.com>
2022-04-30 13:21:54 +0300

    Update calculator JS

---
+++ calculator.js
@@ -0,0 +1,27 @@
+
+        function myFunction() {
+            
+            let x = document.getElementById("unu").value;
+            let y = document.getElementById("doi").value;
+            let z = (y - x)/x * 100 +" %";
+            
+    
+            document.getElementById("apasa").innerHTML = z;
+        }
+        
+        function myReset(){
+             document.getElementById("unu").value = "";
+             document.getElementById("doi").value = "";
+             document.getElementById("apasa").innerHTML = "";
+        }
+    
+        document.addEventListener("keyup", function(event) {
+            if (event.keyCode === 13) {
+            document.getElementById("buton").click();
+        }
+    });
+        document.addEventListener("keyup", function(event) {
+        if (event.keyCode === 82) {
+            document.getElementById("reset").click();
+        }
+    });
